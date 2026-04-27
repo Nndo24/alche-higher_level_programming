28abfdcf2428595bede2999c78ff9af5add737ba
Nndo24 <n.ukoh@alustudent.com>
2026-04-27 05:31:04 -0700

    Everything can be imported

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """
+    Import the variable 'a' from variable_load_5.py 
+    and print its value.
+    """
+    from variable_load_5 import a
+
+    print(a)
