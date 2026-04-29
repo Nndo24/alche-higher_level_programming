fe034a385dcf99d6984db4f4b6eabcd3be4d80e6
Nndo24 <n.ukoh@alustudent.com>
2026-04-29 10:35:56 -0700

    Add tasks 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
-    
-    biggest = my_list[0]
-    for x in my_list:
-        if x > biggest:
-            biggest = x
-    return biggest
+
+    max_value = my_list[0]
+
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+
+    return max_value
