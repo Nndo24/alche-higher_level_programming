cbf19bd4a265c99cee317aafc72923345819bc49
Nndo24 <n.ukoh@alustudent.com>
2026-08-28 00:20:20 -0700

    Add PEP8 compliant 4-inherits_from.py implementation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""Module for inherits_from function."""
+"""
+Module 4-inherits_from
+Contains function inherits_from
+"""
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj inherited (subclass) from a_class; otherwise False."""
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
