e9434e5471ab75dc5216a1d51ce312a2966915b4
Nndo24 <n.ukoh@alustudent.com>
2026-09-18 07:29:02 -0700

    Fix 4-header.sh cURL flags

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with header X-HolbertonSchool-User-Id: 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a header variable
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
