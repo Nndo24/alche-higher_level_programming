29ecd261d99c1719ea67cb9cfb44f3c402185c6b
Nndo24 <n.ukoh@alustudent.com>
2026-09-18 20:34:27 -0700

    Add -L flag to follow redirects in 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a header variable
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a header variable and follows redirects
+curl -sL -H "X-HolbertonSchool-User-Id: 98" "$1"
